c8c387d65e31cbc7d9c2c4968a03bc68ba06505c
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-23 17:53:27 -0500

    added TextField component

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { ButtonGroup, FormControlLabel } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField  from '@material-ui/core/TextField';
+
+
+
+const CheckBoxExample = () => {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <FormControlLabel
+      control= {<Checkbox
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label': 'primary checkbox',
+        }}
+      />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TextField variant="filled" color="secondary" type="email" label="The Time" placeholder="" />
+        <CheckBoxExample />
+        <ButtonGroup variant="contained" color="primary">
         <Button
             startIcon={<SaveIcon />}
-            endIcon={<SaveIcon />}
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{
-              fontSize: 20,
-              backgroundColor: 'purple',
-            }}
             >
-          Hello World
+          Save
         </Button>
+        <Button
+            startIcon={<DeleteIcon />}
+            >
+         Discard
+        </Button>
+        </ButtonGroup>
+
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
