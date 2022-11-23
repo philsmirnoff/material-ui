344c7cd6f246bd6cb9404adbb572e76d56b3f14f
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-23 17:01:16 -0500

    added save icon from material ui icon library

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button
+            startIcon={<SaveIcon />}
+            endIcon={<SaveIcon />}
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{
+              fontSize: 20,
+              backgroundColor: 'purple',
+            }}
+            >
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
